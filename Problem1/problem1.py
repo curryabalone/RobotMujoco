f7aeec00d080e270f0e168467da5cd89224bff84
curryabalone <145168341+curryabalone@users.noreply.github.com>
2025-10-14 18:16:11 -0400

    newest

--- Problem1/problem1.py
+++ Problem1/problem1.py
@@ -1,10 +1,10 @@
-import pinocchio as pin
 import numpy as np
 import mujoco
 import time
 import math
 from mujoco import viewer
-from dmmotor import motor
+from .. import dmmotor
+from dmmotor import dmmotor
 
 def calculate_gravity_torque(motorangle):
     return math.cos(motorangle)*9.81*(0.142*0.15 + 0.362*0.3) 
@@ -20,24 +20,17 @@ def main():
     cur_time = 0
     while True:
         tmp = mj_data.qpos[0]
-        motordelta = tmp - motorangle
-        motorangle = tmp
-        motorspeed = motordelta/0.001
-        exernal_torque = calculate_gravity_torque(motorangle)
-        #let's try to write a simple dampening program!
         flag = False #flag to indiciate if calculations have been done during hold
         friction_torque = 0
+        applied_torque = 0
+        shaft_weight = 0.1 #change later
         while(time.time() - cur_time < 0.001):
             if(flag == False):
                 flag = True 
-                motor.friction_torque(mj_data.qvel[0], 0.362*9.81, exernal_torque) #change parameters later
+                motor.friction_torque(mj_data.qvel[0], mj_data.qvel[0], applied_torque) #change parameters later
                 #complete calculations during hold
-                
-            x = 1 + 1
+            x = 1 + 1 #dummy operations
         mujoco.mj_step(mj_model, mj_data)
-        friction_torque = 0 #torque due to friction, to be calculated
-        if mj_data.qvel[0] == 0.0:
-            friction_torque = 
         cur_time = time.time()
         viewer_window.sync()
 main()
--- dmmotor.py
+++ dmmotor.py
@@ -1,11 +1,11 @@
 import numpy as np
-class motor:
+class dmmmotor:
     def __init__(self, mu_static, mu_viscious, mu_coulumb):
         self.mu_static = mu_static
         self.mu_viscious = mu_viscious
         self.mu_coulumb = mu_coulumb
     def friction_torque(self, velocity, shaft_load, external_torque):
         if velocity == 0 and external_torque < self.mu_static * shaft_load:
-            return self.mu_static * shaft_load * np.sign(external_torque) #static friction should always oppose the external torque
+            self.friction_tau = self.mu_static * shaft_load * np.sign(external_torque) #static friction should always oppose the external torque
         else:
             return 0 #change this later
